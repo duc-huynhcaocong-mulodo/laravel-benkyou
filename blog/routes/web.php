c81316dcffc2e387b20a2e7cfcd35c24bc3a655f
Duc <caocongduc95@gmail.com>
2017-09-26 16:23:39 +0700

    Lesson 11

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -6,7 +6,10 @@ Route::get('/tasks', 'TasksController@index');
 Route::get('tasks/{task}', 'TasksController@show');
 
 Route::get('/', 'PostsController@index');
-Route::get('/post/{post}', 'PostsController@show');
+//Route::get('/post/{post}', 'PostsController@show');
+Route::get('/post/create', 'PostsController@create');
+
+Route::post('/post', 'PostsController@store');
 
 
 //Route::get('/tasks', function () {

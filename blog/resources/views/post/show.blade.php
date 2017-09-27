0f25559d11b8e050486ddc166937f72cfb788515
Duc <caocongduc95@gmail.com>
2017-09-27 17:31:51 +0700

    Lesson 15

--- blog/resources/views/post/show.blade.php
+++ blog/resources/views/post/show.blade.php
@@ -5,5 +5,26 @@
   <h1>{{ $post->title }}</h1>
 
   {{ $post->body }}
+
+  <hr>
+  <div class="comments">
+
+    <ul class="list-group">
+      <?php foreach ($post->comments as $comment): ?>
+
+
+        <li class="list-group-item">
+          <article>
+            <strong>
+              {{ $comment->created_at->diffForHumans() }}: &nbsp;
+            </strong>
+            
+            {{ $comment->body }}
+          </article>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+
 </div>
 @endsection

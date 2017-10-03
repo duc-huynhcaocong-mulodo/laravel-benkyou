2eb3d967c47fedf956d0abc278cfa3b3cd9d4d8a
Duc <caocongduc95@gmail.com>
2017-10-03 15:22:36 +0700

    Lesson 19

---
+++ blog/app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Hash;
+
+class RegistrationController extends Controller
+{
+  public function create(){
+      return view('registration.create');
+  }
+
+  public function store(){
+    //Validate the form
+    $this->validate(request(), [
+      'name' => 'required',
+      'email' => 'required|email',
+      'password' => 'required|confirmed'
+    ]);
+    //Create and save the user
+    $name = request('name');
+    $email = request('email');
+    $password = Hash::make(request('password'));
+    $user = User::create(['name' => $name,
+                        'email' => $email,
+                        'password' => $password]);
+    //Sign them in
+    auth()->login($user);
+    //Redicrect to home
+    return redirect()->home();
+  }
+}
---
+++ blog/resources/views/sessions/create.blade.php
@@ -0,0 +1,30 @@
+
+
+@extends ('layout.master')
+
+@section ('content')
+  <div class="col-sm-8">
+    <h1>Sign In</h1>
+
+    <hr>
+    <form method="post" action="/login">
+      {{ csrf_field() }}
+
+        <div class="form-group">
+          <label for="email">Email Address:</label>
+          <input type="email" class="form-control" id="email" name="email" />
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" class="form-control" id="password" name="password" />
+        </div>
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Sign In</button>
+        </div>
+
+        @include('layout.errors')
+    </form>
+  </div>
+@endsection

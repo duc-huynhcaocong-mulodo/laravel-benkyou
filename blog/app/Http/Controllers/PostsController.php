7a2d21d40fd7770ae2129ba1c558bc265c846bab
Duc <caocongduc95@gmail.com>
2017-10-06 14:44:48 +0700

    Lesson 21

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -17,13 +17,9 @@ class PostsController extends Controller
               ->get();
 
       //$posts = $posts->get();
-      $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-              ->groupBy('year', 'month')
-              ->orderByRaw('min(created_at) desc')
-              ->get()
-              ->toArray();
+      //$archives = Post::archives();
 
-      return view('post.index', compact('posts', 'archives'));
+      return view('post.index', compact('posts'));
       //return view('post.index');
     }
 
